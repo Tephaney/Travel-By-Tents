bfc43e22186508d4f416b515a1a502efdf3825e1
Tephaney <101478420+Tephaney@users.noreply.github.com>
2022-08-08 14:03:29 -0700

    Send Email and Social Sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,5 +1,12 @@
 import { useRef } from "react";
-import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+  Alert,
+  Share,
+} from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from "react-native-animatable";
@@ -10,7 +17,6 @@ const RenderCampsite = (props) => {
   const view = useRef();
 
   const isLeftSwipe = ({ dx }) => dx < -200;
-
   const isRightSwipe = ({ dx }) => dx > 200;
 
   const panResponder = PanResponder.create({
@@ -50,10 +56,23 @@ const RenderCampsite = (props) => {
     },
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title,
+        message: `${title}: ${message} ${url}`,
+        url,
+      },
+      {
+        dialogTitle: "Share " + title,
+      }
+    );
+  };
+
   if (campsite) {
     return (
       <Animatable.View
-        animation="fadeInDown"
+        animation="fadeInDownBig"
         duration={2000}
         delay={1000}
         ref={view}
@@ -85,7 +104,21 @@ const RenderCampsite = (props) => {
               color="#5637DD"
               raised
               reverse
-              onPress={() => props.onShowModal()}
+              onPress={props.onShowModal}
+            />
+            <Icon
+              name="share"
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() =>
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
