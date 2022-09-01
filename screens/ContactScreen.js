8ffef009ac063012bc09eb31a05980e7a86f2520
Tephaney <101478420+Tephaney@users.noreply.github.com>
2022-08-31 20:28:22 -0700

    Address

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -18,11 +18,11 @@ const ContactScreen = () => {
         <Card wrapperStyle={{ margin: 20 }}>
           <Card.Title>Contact Information</Card.Title>
           <Card.Divider />
-          <Text>1 Nucamp Way</Text>
+          <Text>1 Travel Way</Text>
           <Text>Seattle, WA 98001</Text>
           <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
           <Text>Phone: 1-206-555-1234</Text>
-          <Text>Email: campsites@nucamp.co</Text>
+          <Text>Email: campsites@travelbytents.com</Text>
           <Button
             title="Send Email"
             buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
